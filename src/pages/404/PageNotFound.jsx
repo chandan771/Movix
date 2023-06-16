3f9866b6ade50baac98559056086e6461dfd92e6
chandan771 <111229538+chandan771@users.noreply.github.com>
2023-06-16 10:31:18 +0530

    Update PageNotFound.jsx

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+
 import "./fort.scss";
 
+import ContentWrapper from "../../components/contentWrap/ContentWrapper";
+
 const PageNotFound = () => {
-  return (
-    <div>
-      <h2>Foot</h2>
-    </div>
-  );
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
 };
 
 export default PageNotFound;
